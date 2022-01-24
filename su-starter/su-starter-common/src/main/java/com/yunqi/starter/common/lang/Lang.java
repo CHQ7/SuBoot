1ee4b49b8f5fcea3cb6695729cfadac519fe9e8b
JsckChin <1259900@qq.com>
2022-01-24 21:53:30 +0800

    更新版本

---
+++ su-starter/su-starter-common/src/main/java/com/yunqi/starter/common/lang/Lang.java
@@ -0,0 +1,14 @@
+package com.yunqi.starter.common.lang;
+
+
+
+/**
+ * <p>
+ *     这些帮助函数让 Java 的某些常用功能变得更简单
+ * </p>
+ * Created by @author JsckChin on 2022/1/22
+ */
+public abstract class Lang {
+
+
+}
