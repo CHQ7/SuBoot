27139e482b5a335946c5250606e81ee978734e82
CHQ <1259900@qq.com>
2022-05-04 15:37:11 +0800

    add:更新项目

--- su-admin/src/test/java/com/yunqi/Tests.java
+++ su-admin/src/test/java/com/yunqi/Tests.java
@@ -1,7 +1,10 @@
 package com.yunqi;
 
+import com.yunqi.starter.common.lang.Strings;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.regex.Pattern;
 
 
 /**
@@ -12,7 +15,7 @@ class Tests {
 
     @Test
     public void test(){
-
+        System.out.println(Strings.unicodeDecode("\\u5df2\\u89e3\\u5c01"));
     }
 
 }
